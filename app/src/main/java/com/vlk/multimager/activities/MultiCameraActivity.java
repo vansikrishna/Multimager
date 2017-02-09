29703d27ef1f43c2e391db0faf45c7c7911721fc
bhaskarpakki <bhaskar.pakki@xinthe.com>
2017-02-09 16:13:50 +0530

    Initial commit

---
+++ app/src/main/java/com/vlk/multimager/activities/MultiCameraActivity.java
@@ -0,0 +1,80 @@
+package com.vlk.multimager.activities;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.vlk.multimager.R;
+import com.vlk.multimager.utils.Constants;
+import com.vlk.multimager.utils.Params;
+
+/**
+ * Created by vansikrishna on 6/14/2016.
+ */
+public class MultiCameraActivity extends BaseActivity{
+
+    Params params;
+    Fragment fragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_capture);
+        init();
+        initFragment();
+    }
+
+    private void init(){
+        if(this.getIntent() != null){
+            if(this.getIntent().hasExtra(Constants.KEY_PARAMS)) {
+                Object object = this.getIntent().getSerializableExtra(Constants.KEY_PARAMS);
+                if(object instanceof Params)
+                    params = (Params) object;
+                else{
+                    Toast.makeText(this, "Provided serializable data is not an instance of Params object.", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(fragment instanceof CameraFragment){
+            ((CameraFragment) fragment).onBackPressed();
+        }
+        else{
+            ((Camera2Fragment) fragment).onBackPressed();
+        }
+    }
+
+    private void initFragment(){
+        fragment = CameraFragment.newInstance(params);
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+//        if(Build.VERSION.SDK_INT >= 21){
+//            fragment = Camera2Fragment.newInstance(params);
+//            getFragmentManager().beginTransaction()
+//                    .replace(R.id.container, fragment)
+//                    .commit();
+//        }
+//        else{
+//            fragment = CameraFragment.newInstance(params);
+//            getFragmentManager().beginTransaction()
+//                    .replace(R.id.container, fragment)
+//                    .commit();
+//        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/vlk/multimager/utils/Constants.java
@@ -0,0 +1,19 @@
+package com.vlk.multimager.utils;
+
+/**
+ * Created by vansikrishna on 08/06/2016.
+ */
+public class Constants {
+
+    public static final int REQUEST_STORAGE_PERMS = 1;
+    public static final int REQUEST_CAMERA_PERMISSION = 2;
+    public static final int REQUEST_MULTIPLE_PERMISSIONS = 3;
+    public static final int REQUEST_CALL_PERMS = 4;
+
+    public static final int TYPE_MULTI_PICKER = 3;
+    public static final int TYPE_MULTI_CAPTURE = 4;
+
+    public static final String KEY_BUNDLE_LIST = "BUNDLE_LIST";
+    public static final String KEY_PARAMS = "PARAMS";
+
+}
